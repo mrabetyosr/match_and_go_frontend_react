977afcf6c12388b00869599c1e4e671af1d68ab1
Yosr Mrabet <yosr.mrabet@esprit.tn>
2025-09-06 00:46:36 +0100

    remove sidebar component

--- src/layouts/AdminLayout/AdminLayout.jsx
+++ src/layouts/AdminLayout/AdminLayout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import SideBar from "../../components/SideBar/SideBar";
+
 
 const AdminLayout = ({ children }) => {
   return (
     <div className="flex h-screen">
       {/* Sidebar gauche */}
-      <SideBar />
+      
 
       {/* Contenu principal */}
       <div className="flex-1 bg-gray-100 p-6 overflow-auto">
